8bc81464a50fe285042639e9669a076338ab89e1
potydev <xysuba0@gmail.com>
2026-08-14 16:15:57 +0700

    fix: remove duplicate app declaration in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql2/promise';
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
 import path from 'path';
 import { fileURLToPath } from 'url';
 
